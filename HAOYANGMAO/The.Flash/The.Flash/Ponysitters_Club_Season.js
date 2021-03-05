685e8233d0715e7e49fbe6eef8188c3f5b0c0c52
ZHUZHU06 <79742684+ZHUZHU06@users.noreply.github.com>
2021-03-05 19:20:11 +0800

    Update Ponysitters_Club_Season.js

--- HAOYANGMAO/The.Flash/The.Flash/Ponysitters_Club_Season.js
+++ HAOYANGMAO/The.Flash/The.Flash/Ponysitters_Club_Season.js
@@ -212,7 +212,10 @@ return st
 }
 
 
-
+function shareCodesFormat() {
+return new Promise(async resolve => {
+  // console.log(`第${$.index}个京东账号的助力码:::${$.shareCodesArr[$.index - 1]}`)
+  $.newShareCodes = [];
 
 
 

8ff145ee706cb5abdcb297887e35760a6b4e1472
ZHUZHU06 <79742684+ZHUZHU06@users.noreply.github.com>
2021-03-07 20:26:38 +0800

    Update Ponysitters_Club_Season.js

--- Ponysitters_Club_Season.js
+++ Ponysitters_Club_Season.js
@@ -3,7 +3,7 @@
 // detail url: https://github.com/ruicky/jd_sign_bot
 /*
 2020.3.5增加内置互助码填写
-2020.3.7修复github 日志log显示不了多账号
+2020.3.7修复github 日志log显示不了多账号,增加惊喜农场不需要运行的账号跳过功能
 
 　BBC2历史电视电影系列《空王冠》（The Hollow Crown）4部经典莎士比亚作品的新演绎，分别是：《理查二世》（Richard II）、《亨利四世：第一部》（Henry IV, Part 1）、《亨利四世：第二部》（Henry IV, Part 2）和《亨利五 世》（Henry V）。在2012年伦敦奥运会期间，为了向全世界展现英国文化，这四部电视电影将作为伦敦文化奥运的重点推荐剧目向全世界推行，在2012年6月播出。　　《亨利四世》是该系列的第二部。夺取理查二世权势的亨利四世，将由奥斯卡影帝杰瑞米·艾恩斯 Jeremy Irons饰演，将由《丑闻纪事》的导演理查德·艾尔掌镜，在《复仇者联盟》中雷神弟弟“洛基”汤姆·希德勒斯顿 Tom Hiddleston将饰演Prince Hal，西蒙·拉塞尔·比尔 Simon Russell Beale饰演约翰·福斯塔夫爵士，艾伦·阿姆斯特朗 Alun Armstrong饰演诺森伯兰伯爵。　　《亨利四世》展现了英国中世纪时期迷人的历史，并将演绎出近年来最野心勃勃的一部莎士比亚改编作品。《亨利四世》是莎士比亚历史剧中最成功、最受欢迎的一部，被看成莎士比亚历史剧的代表作。这部作品的主要内容是反映亨利四世和他的王子们与反叛的诸侯贵族进行殊死斗争的过程。莎士比亚突破传统历史剧多条线索交织发展的网状结构，采用了两条线索平行发展的结构——以亨利四世为代表的宫廷生活线索和以福斯塔夫为代表的市井生活线索。
 
@@ -50,7 +50,7 @@ const JXNCTOKENS = process.env.JXNCTOKENS; //京戏农场种子
 
 
 
-
+let jxnc_nodo=[];//惊喜农场固定跳过号码,格式jxnc_nodo=[0,1],意思是跳过第1个,第2个号码不执行.
 
 
 let NCShareCodes=[""];//京东农厂
@@ -102,6 +102,17 @@ async function executeOneByOne() {
     const content = await fs.readFileSync("./temp.js", "utf8");
     for (var i = 0; i < CookieJDs.length; i++) {
         console.log(`正在执行第${i + 1}个任务`);
+  if (process.env.SYNCURL.indexOf('j0xn0c.js')>0)
+{
+  if (jxnc_nodo.indexOf(i)>=0)
+      {
+  console.log(`惊喜农场默认设置,跳过第${i + 1}个账号`);
+      continue;}
+      
+      
+ }      
+        
+        
         changeFiele(content, CookieJDs[i]);
         $.UserName = decodeURIComponent(CookieJDs[i].match(/pt_pin=(.+?);/) && CookieJDs[i].match(/pt_pin=(.+?);/)[1])
         $.index = i + 1;
